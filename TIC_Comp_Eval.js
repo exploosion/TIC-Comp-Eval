6c73245676e1185b905475e94e2afb21810f5103
Yu Hsu <yhsu@kbbh.org>
2022-04-25 08:29:08 -0700

    Added validation for arrests fields.

--- TIC_Comp_Eval.js
+++ TIC_Comp_Eval.js
@@ -340,3 +340,19 @@ $('document').ready(function(){
 	$('tr').has('div[id=arrestsTotal]').find('input').change(checkArrests);
 	$('tr').has('div[class=arrests]').find('input').change(checkArrests);
 });
+
+//Checks for arrests
+$('document').ready(function(){
+	$('input[name=Complete]').prop('disabled', false);
+	$('input[name=Complete]').click(function(e){
+		if($('tr').has('div[id=duiiArrests]').find('input').val() < $('tr').has('div[id=duiiArrestsPastMonth]').find('input').val()){
+			e.preventDefault();
+			alert('Total DUII Arrests have to be greater than or equal to Number of DUII Arrests in Past Month');
+		}
+		
+		if($('tr').has('div[id=arrests]').find('input').val() < (parseInt($('tr').has('div[id=duiiArrests]').find('input').val()) + parseInt($('tr').has('div[id=arrestsPastMonth]').find('input').val()))){
+			e.preventDefault();
+			alert('Total Arrests have to be greater than or equal to Total DUII Arrests and Number of Arrests in Past Month');
+		}
+	});
+});
