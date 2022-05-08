9fb95389539c03729b714e0349d8f769380793e0
Yu Hsu <yhsu@kbbh.org>
2022-05-08 12:36:40 -0700

    Added ACE.

--- TIC_Comp_Eval.js
+++ TIC_Comp_Eval.js
@@ -597,6 +597,95 @@ $('document').ready(function(){
 	});
 });
 
+//ACE
+var acePreviousScore;
+
+function checkACEPreviousScore(){
+	if($('tr').has('div[id=acePrevious]').find('input')?.val() != ''){
+		acePreviousScore = $('tr').has('div[id=acePrevious]').find('b')[0]?.innerHTML;
+	}
+	if(!$.isNumeric(acePreviousScore)){
+		$('tr').has('div[id=acePrevious]').find('div').eq(1).text('9001');
+		acePreviousScore = 9001;
+	}
+}
+
+function createACE(){
+	$('tr').has('div[id=ace]').eq(0).next().after('<div id=\'aceTest\'><div id=\'aceContainer\'></div><br><img src=\'/images/spacer.gif\' height=\'15\' width=\'1\'></div>');
+
+	$('div[id=aceContainer]').html('<b>ACE</b><br><br><i>While the client was groing up, during their first 18 years of life:</i></td></tr>' + '<table border=\'0\' cellspacing=\'10px\'>' +
+		'<tr><td><label for=aceQ1 class=\'aceLabels\'><b>Did a parent or other adult in the household often: swear at, insult, put down, or humiliate them; or act in a way that made them afraid that they might be physically hurt?</b></label></td><td><select id=\'aceQ1\' class=\'aceQuestions\'><option value=\'\' selected disabled hidden>Select an Option</option><option value=\'Yes\'>Yes</option><option value=\'No\'>No</option></select></td></tr>' +
+		'<tr><td><label for=aceQ2 class=\'aceLabels\'><b>Did a parent or other adult in the household often: push, grab, slap, throw something at them; or ever hit them so hard that they had marks or were injured?</b></label></td><td><select id=\'aceQ2\' class=\'aceQuestions\'><option value=\'\' selected disabled hidden>Select an Option</option><option value=\'Yes\'>Yes</option><option value=\'No\'>No</option></select></td></tr>' +
+		'<tr><td><label for=aceQ3 class=\'aceLabels\'><b>Did an adult or person at least 5 years older than them ever: touch or fondle them or had them touch their body in a sexual way; or try to or actually had oral, anal, or vaginal sex with them?</b></label></td><td><select id=\'aceQ3\' class=\'aceQuestions\'><option value=\'\' selected disabled hidden>Select an Option</option><option value=\'Yes\'>Yes</option><option value=\'No\'>No</option></select></td></tr>' +
+		'<tr><td><label for=aceQ4 class=\'aceLabels\'><b>Did they often feel that: no one in their family loved them or thought they were important or special; or their family didn\'t look out for each other, feel close to each other, or support each other?</b></label></td><td><select id=\'aceQ4\' class=\'aceQuestions\'><option value=\'\' selected disabled hidden>Select an Option</option><option value=\'Yes\'>Yes</option><option value=\'No\'>No</option></select></td></tr>' +
+		'<tr><td><label for=aceQ5 class=\'aceLabels\'><b>Did they often feel that: they didn\'t have enough to eat, had to wear dirty clothes, and had no one to protect them; or their parents were too drunk or high to take care of them or take them to the doctor if they needed it?</b></label></td><td><select id=\'aceQ5\' class=\'aceQuestions\'><option value=\'\' selected disabled hidden>Select an Option</option><option value=\'Yes\'>Yes</option><option value=\'No\'>No</option></select></td></tr>' +
+		'<tr><td><label for=aceQ6 class=\'aceLabels\'><b>Were their parents ever separated or divorced?</b></label></td><td><select id=\'aceQ6\' class=\'aceQuestions\'><option value=\'\' selected disabled hidden>Select an Option</option><option value=\'Yes\'>Yes</option><option value=\'No\'>No</option></select></td></tr>' +
+		'<tr><td><label for=aceQ7 class=\'aceLabels\'><b>Was their mother or stepmother: often pushed, grabbed, slapped, or had something thrown at her; or sometimes, often, or very often kicked, bitten, hit with a fist, or hit with something hard; or ever repeatedly hit over at least a few minutes; or threatened with a gun or knife?</b></label></td><td><select id=\'aceQ7\' class=\'aceQuestions\'><option value=\'\' selected disabled hidden>Select an Option</option><option value=\'Yes\'>Yes</option><option value=\'No\'>No</option></select></td></tr>' +
+		'<tr><td><label for=aceQ8 class=\'aceLabels\'><b>Did they live with anyone who was a problem drinker or alcoholic of who used street drugs?</b></label></td><td><select id=\'aceQ8\' class=\'aceQuestions\'><option value=\'\' selected disabled hidden>Select an Option</option><option value=\'Yes\'>Yes</option><option value=\'No\'>No</option></select></td></tr>' +
+		'<tr><td><label for=aceQ9 class=\'aceLabels\'><b>Was a household member depressed or mentally ill or did a household member attempt suicide?</b></label></td><td><select id=\'aceQ9\' class=\'aceQuestions\'><option value=\'\' selected disabled hidden>Select an Option</option><option value=\'Yes\'>Yes</option><option value=\'No\'>No</option></select></td></tr>' +
+		'<tr><td><label for=aceQ10 class=\'aceLabels\'><b>Did a household member go to prison?</b></label></td><td><select id=\'aceQ10\' class=\'aceQuestions\'><option value=\'\' selected disabled hidden>Select an Option</option><option value=\'Yes\'>Yes</option><option value=\'No\'>No</option></select></td></tr>' +		
+		'</table>');
+}
+
+function checkACEPerform(){
+	if($('answer[id=acePerform]').parent().prev().find('input').prop('checked')){
+		$('div[id=aceTest]').show();
+		$('.aceQuestions').attr('required', true);
+		if(!$('div[id=aceTest]').find('div[class=requiredAsterisk]').length){
+			$('div[id=aceTest]').find('label[class=aceLabels]').find('b').after('<div class=\'requiredAsterisk\' style=\'color:red;display:inline\'>*</div>');
+		}
+		$('tr').has('div[id=aceScore]').find('input').prop('readonly', true);
+	}
+	else if($('answer[id=acePreviousScore]').parent().prev().find('input').prop('checked')){
+		$('div[id=aceTest]').hide();
+		$('.aceQuestions').attr('required', false);
+		$('div[id=aceTest]').find('div[class=requiredAsterisk]').remove();
+		$('.aceQuestions').val('');
+		$('tr').has('div[id=aceScore]').find('input').prop('readonly', true);
+		$('tr').has('div[id=aceScore]').find('input').val(acePreviousScore);
+	}
+	else if($('answer[id=aceManual]').parent().prev().find('input').prop('checked')){
+		$('div[id=aceTest]').hide();
+		$('.aceQuestions').attr('required', false);
+		$('div[id=aceTest]').find('div[class=requiredAsterisk]').remove();
+		$('.aceQuestions').val('');
+		$('tr').has('div[id=aceScore]').find('input').prop('readonly', false);
+		if($('tr').has('div[id=aceScore]').find('input').val() != ''){
+			$('tr').has('div[id=aceScore]').find('input').val('');
+		}
+	}
+	else{
+		$('div[id=aceTest]').hide();
+		$('.aceQuestions').attr('required', false);
+		$('div[id=aceTest]').find('div[class=requiredAsterisk]').remove();
+		$('.aceQuestions').val('');
+		$('tr').has('div[id=aceScore]').find('input').prop('readonly', false);
+	}
+}
+
+function calculateACE(){
+	var scoreTotal = 0;
+	var scoreAverage = 0;
+
+	$('.aceQuestions').each(function(){
+		console.log($(this).val());
+		if($(this).val() != '' && $(this).val() != 'Did Not Answer'){
+			scoreTotal = scoreTotal + parseInt($(this).val());
+		}
+	})
+
+	$('tr').has('div[id=aceScore]').find('input').val(scoreAverage);
+}
+
+$('document').ready(function(){
+	checkACEPreviousScore();
+	createACE();
+	checkACEPerform();
+
+	$('tr').has('div[id=ace]').find('input').change(checkACEPerform);
+	$('.ACEQuestions').change(calculateACE);
+});
+
 //Living Arrangement
 var livingArrangement;
 function checkLivingArragement(){
