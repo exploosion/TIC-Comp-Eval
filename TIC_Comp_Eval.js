c14ded03a2c76517db6f0791067cd51e375976bf
Yu Hsu <yhsu@kbbh.org>
2022-05-09 09:14:48 -0700

    Depression Screening Questions.

--- TIC_Comp_Eval.js
+++ TIC_Comp_Eval.js
@@ -808,7 +808,7 @@ function checkPositivePHQ9(){
 	hideShow('hide', 'depressionScreeningAdolescent', false);
 	hideShow('hide', 'depressionScreeningAdult', false);
 	hideShow('show', 'sra', false);
-	if(phq9Score < 9){
+	if(phq9Score < 9 && phq9Score != undefined){
 		hideShow('show', 'positiveScreening', true);
 		hideShow('show', 'sra', true);
 		if(age > 10 && age < 18){
